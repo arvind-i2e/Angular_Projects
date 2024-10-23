87d1e513186020852bbedc8318c3496a86f16a44
arvind-i2e <arvind.singh@i2econsulting.com>
2024-10-23 17:15:28 +0530

    Update in  home.component.ts

--- ArvindHomesForSale/src/app/home/home.component.ts
+++ ArvindHomesForSale/src/app/home/home.component.ts
@@ -47,7 +47,7 @@ export class HomeComponent {
 
     this.filteredLocationList = this.housingLocationList.filter(
       (housingLocation) =>
-        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+        housingLocation?.city?.toLowerCase().includes(text.toLowerCase())
     );
   }
 }
